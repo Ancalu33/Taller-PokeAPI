2d2f8a8122f44722fb2013a1cb3b982ad6dac1ad
Andres Lugo <ancalu33@hotmail.com>
2020-10-05 14:42:50 -0500

    PokeApi funcionando
    Pinta varios pokemon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,8 +12,8 @@ const btnBuscar = document.getElementById('btnBuscar')
 btnBuscar.addEventListener('click', () => {
     let pkmNumber = document.getElementById('inputPkmn').value
     pkmNumber = pkmNumber.split(" ")
-    
-    
+
+
     if (pkmNumber != "") {
         pkmNumber.forEach((pk) => {
             console.log(pk);
@@ -27,10 +27,13 @@ btnBuscar.addEventListener('click', () => {
 
 
 const getData = (api) => {
+    let html = "";
+
+    document.getElementById("tarjetaPokemon").innerHTML = "";
     return fetch(api)
         .then((response) => response.json())
         .then((json) => {
-            llenarDatos(json);
+            llenarDatos(json, html);
         })
         .catch((error) => {
             console.log("Error :", error)
@@ -48,9 +51,8 @@ const getData = (api) => {
         })
 } */
 
-const llenarDatos = (data) => {
+const llenarDatos = (data,html) => {
     console.log(data)
-    let html = "";
 
     let typeP = ""
     data.types.forEach(tPkm => {
@@ -62,7 +64,7 @@ const llenarDatos = (data) => {
 
     html += '<div class="col-2 mx-2 my-3 ">';
     html += '<div class="card myCard p-2 shadow" style="width: 12rem; ">';
-    html += `<img src="${data.sprites.front_default}" class="card-img-top img-thumbnail">`
+    html += `<img src="${data.sprites.other.dream_world.front_default}" class="card-img-top img-thumbnail">`
     html += `<h5 class="card-title font-weight-bold">Nombre: <span class="font-weight-light text-uppercase"> ${data.name} </span></h5>`
     html += `<span class="card-text font-weight-bold">Id: </span><span class="font-weight-light ">${data.id}</span> `;
     html += `<span class="card-text font-weight-bold">Especie: </span><span class="font-weight-light text-capitalize"> ${typeP}</span>  `;
@@ -71,55 +73,5 @@ const llenarDatos = (data) => {
     html += '</div>';
     html += '</div>';
 
-    document.getElementById('tarjetaPokemon').innerHTML = html;
+    document.getElementById('tarjetaPokemon').innerHTML += html;
 }
-
-
-/* 
-
-const llenarDatos = (data) => {
-    let html = "";
-    data.results.forEach((pj) => {
-        html += '<div class="col-2 mx-2 my-3">';
-        html += '<div class="card myCard p-2" style="width: 12rem;">';
-        html += `<img src="${pj.image}" class="card-img-top img-thumbnail">`
-        html += `<h5 class="card-title">Nombre: ${pj.name}</h5>`
-        html += `<p class="card-text">Estado: ${pj.status}  </p>`;
-        html += `<p class="card-text">Especie: ${pj.species}  </p>`;
-        html += `<p class="card-text">Genero: ${pj.gender}  </p>`;
-      
-        html += '</div>';
-        html += '</div>';
-        html += '</div>';
-
-    });
-    document.getElementById('datosPersonajes').innerHTML = html;
-} */
-
-//    paginacion
-/* 
-const paginacion = (info) => {
-
-    let prevDisabled = "";
-    let nextDisabled = "";
-
-    if (info.prev == null) {
-        prevDisabled = "disabled";
-    } else {
-        prevDisabled = "";
-    }
-
-    if (info.next == null) {
-        nextDisabled = "disabled";
-    } else {
-        nextDisabled = "";
-    }
-
-
-    let html = "";
-    html += `<li class="page-item ${prevDisabled}"><a class="page-link" onclick="getData('${info.prev}')">Previous</a></li>`
-    html += `<li class="page-item ${nextDisabled}"><a class="page-link" onclick="getData('${info.next}')">Next</a></li>`
-    document.getElementById('paginacion').innerHTML = html;
-
-
-} */
